934537381db7c9f46a476f87fe73abb576f586b6
graphieros <alec.lloyd.probert@gmail.com>
2020-08-07 12:41:36 +0200

    visualize your sticker on a laptop on click

--- logo/events.js
+++ logo/events.js
@@ -44,7 +44,9 @@
     stickers_list.forEach(sticker => {
         let sticker_svg = sticker.getElementsByTagName("svg");
         let svg_copy = sticker_svg[0].cloneNode(true);
+        let original_id = sticker_svg[0].id;
         svg_copy.id = 'temp_svg';
+        let svg_back_color = svg_copy.style.background;
 
         sticker.addEventListener('click', () => {
             let svg_to_delete = showcase.getElementsByTagName("svg")[0];
@@ -52,6 +54,21 @@
             if (svg_to_delete.id === 'temp_svg') {
                 svg_to_delete.remove();
             }
+
+            // hack to compensate the lack of background of callifractal SVGs due to the lazy use of filter for drop-shadow that doesn't show when the SVG background is not transparent:
+
+            if (original_id === 'callifractal_stix_3' || original_id === 'callifractal_stix_5') {
+                standard_laptop_sticker_container.style.background = 'radial-gradient(white,rgb(100,130,150))';
+            } else if (original_id === 'callifractal_stix_7') {
+                standard_laptop_sticker_container.style.background = 'radial-gradient(at top, white, rgb(43,74,33))';
+            } else if (original_id === 'callifractal_stix_9' || original_id === 'callifractal_stix_11') {
+                standard_laptop_sticker_container.style.background = 'radial-gradient(at top, white, rgb(112,73,67))';
+            } else if (original_id === 'callifractal_stix_12' || original_id === 'callifractal_stix_14') {
+                standard_laptop_sticker_container.style.background = 'rgb(255,100,50)';
+            } else {
+                standard_laptop_sticker_container.style.background = svg_back_color;
+            }
+
 
             showcase.style.display = "grid";
             showcase.style.opacity = 1;
