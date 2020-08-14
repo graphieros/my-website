c95fbd4d0a5a1cf252332f84001b3d5459ad608c
graphieros <alec.lloyd.probert@gmail.com>
2020-08-15 01:54:08 +0200

    simple page design proposition

--- reactversion/source/src/components/SubMenuContent/SubMenuBioDo.js
+++ reactversion/source/src/components/SubMenuContent/SubMenuBioDo.js
@@ -7,25 +7,77 @@ const SubMenuBioDo = () => {
     return (
         <div>
             <div className="bioDoTitle">
+                <div id="fractalBioTitleWrapper">
+                    <Fractal className='fractalBioTitle'
+                        sequence='ex-dz-zq-qs-sw'
+                        colors={[255, 255, 255]}
+                        size='30'
+                        svgSize='30'
+                        radius='25'
+                        background=''
+                        light={true}
+                        intensity='1.1'
+                        border=''
+                        backgroundRadius='100%'
+                        dropShadow=''
+                    />
+                </div>
+
                 <span className="spanSmall">Ce que je</span> <b>fais</b>
             </div>
-
-            <div className="bioDoList">
-                <Fractal className='fractalBio0'
-                    sequence='qw-zw-ew-dw-xw'
-                    colors={[10, 10, 10]}
-                    size='20'
-                    svgSize= '10'
-                    radius=''
-                    background=''
-                    light={true}
-                    intensity='1.1'
-                    border=''
-                    backgroundRadius='100%'
-                    dropShadow=''
-                />
+            <div className="bioDoListWrapper">
+                <div id="bioDoList0">
+                    <Fractal className='fractalBio0'
+                        sequence='qw-zw-ew-dw-xw'
+                        colors={[68, 60, 15]}
+                        size='20'
+                        svgSize='10'
+                        radius='10'
+                        background=''
+                        light={true}
+                        intensity='1.1'
+                        border=''
+                        backgroundRadius='100%'
+                        dropShadow=''
+                    />
                 web développement
             </div>
+
+                <div id="bioDoList1">
+                    <Fractal className='fractalBio1'
+                        sequence='zs-se-qd-wx'
+                        colors={[68, 60, 15]}
+                        size='20'
+                        svgSize='10'
+                        radius='10'
+                        background=''
+                        light={true}
+                        intensity='1.1'
+                        border=''
+                        backgroundRadius='100%'
+                        dropShadow=''
+                    />
+                création de langue
+            </div>
+
+                <div id="bioDoList2">
+                    <Fractal className='fractalBio2'
+                        sequence='eq-qx-dq-zs-sw'
+                        colors={[68, 60, 15]}
+                        size='20'
+                        svgSize='10'
+                        radius='10'
+                        background=''
+                        light={true}
+                        intensity='1.1'
+                        border=''
+                        backgroundRadius='100%'
+                        dropShadow=''
+                    />
+                peinture
+            </div>
+            </div>
+
 
         </div>
     )
