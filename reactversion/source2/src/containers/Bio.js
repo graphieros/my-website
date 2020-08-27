e8d7b2dde4e117f4a28169c11affea2e56d9892b
graphieros <alec.lloyd.probert@gmail.com>
2020-08-27 07:33:55 +0200

    basic layout for bio page

--- reactversion/source2/src/containers/Bio.js
+++ reactversion/source2/src/containers/Bio.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import Fractal from '../components/graphieros/Fractal';
+import './css/Bio.css';
+import alp from '../assets/alp.jpg';
 
 const Bio = () => {
-    return(
-        <div>BIO</div>
+    return (
+        <div>
+
+            <div className='bioFractalWrapper'>
+                <Fractal
+                    sequence='we-xs-dz-ze-ed-dx-xw-wq-qz zx-ws-qe ze-eq-xw-wd'
+                    colors={[122,161,216]}
+                    svgSize='300'
+                    light={true}
+                    intensity='2'
+                />
+            </div>
+
+            <div className="biopicWrapper">
+                <img src={alp} alt="alp" />
+            </div>
+
+        </div>
     )
 }
 
