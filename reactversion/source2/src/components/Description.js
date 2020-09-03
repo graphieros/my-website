89d8423ea3040dffa395b789e44bc4809c423df4
graphieros <alec.lloyd.probert@gmail.com>
2020-09-03 03:18:10 +0200

    to be used as a generic presentation container

---
+++ reactversion/source2/src/components/Description.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Description.css';
+
+const Description = (props) => {
+    return(
+        <div className={props.className}>
+            {props.text}
+        </div>
+    )
+}
+
+export default Description;
