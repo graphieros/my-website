bad12cfaad130f31a3dde9df88ba7c375ac0226e
graphieros <alec.lloyd.probert@gmail.com>
2020-09-07 05:42:47 +0200

    added another function for looped shapes

--- reactversion/source2/src/libraries/pairSwap.js
+++ reactversion/source2/src/libraries/pairSwap.js
@@ -1,4 +1,4 @@
-function pairSwap(array) {
+export function pairSwap(array) {
     let aL = array.length;
     let copy = [...array];
     let out = [];
@@ -11,4 +11,40 @@ function pairSwap(array) {
     return out;
 }
 
-export default pairSwap;
+export function changeLoop(array){
+
+    let copy = [...array];
+    let len = copy.length;
+    let start = copy.slice(0,2);
+    let end = copy.slice(len-2, len+1);
+
+    let output = [];
+
+    if(start.toString() === end.toString()){
+
+        output.push([...copy]);
+        //remove last pair
+        let last1 = output[0].pop();
+        let last0 = output[0].pop();
+
+        //remove first pair
+        let first0 = output[0].shift();
+        let first1 = output[0].shift();
+
+        //copy new first pair
+        let newFirst = output[0].slice(0,2);
+        
+        //add new first pair at end
+        output.push(newFirst);
+        output = output.flat();
+
+        //insert ancient pair in second place
+        output.splice(2,0, first1);
+        output.splice(2,0, first0);
+
+        return output;
+    } else {
+        return;
+    }
+
+}
