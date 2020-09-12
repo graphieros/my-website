a5d87b31429809c7ede5d82e4e8aafb2483dc07b
graphieros <alec.lloyd.probert@gmail.com>
2020-09-12 09:25:45 +0200

    added books content

--- reactversion/source2/src/containers/Livres.js
+++ reactversion/source2/src/containers/Livres.js
@@ -2,16 +2,44 @@ import React from 'react';
 import './css/Livres.css';
 import Livre from '../components/Livre';
 import accords from '../assets/books/accords.png';
+import hexagerations from '../assets/books/hexagerations.png';
+import objedtDesir from '../assets/books/objedtDesir.png';
+import cle from '../assets/books/cle.png';
 
 const Livres = () => {
     return(
-        <div className="temp">
+        <div>
+            <div className="textePresentation">
+                <p className="textePresentationTitle">Textes</p>
+                <p>une sélection des livres que j'ai commis durant les deux dernières décennies</p>
+            </div>
              <Livre
                 className='livre'
                 title='Accords'
                 image={accords}
                 imageAlt='accords'
                 description="Comment représenter par écrit des pensées parallèles ? Comment faire en sorte que le lecteur puisse vivre cette expérience de lecture sur plusieurs plans simultanés ? Accords tente de répondre à ces questions avec une écriture sous forme de contrepoints et d'accords."
+            />
+            <Livre
+                className='livre'
+                title='Hexagerations'
+                image={hexagerations}
+                imageAlt='hexagerations'
+                description="Un beau livre où une sélection de peintures façonnées avec le graphieros sont accompagnées par la traduction du texte en français."
+            />
+            <Livre
+                className='livre'
+                title="L'objet du désir"
+                image={objedtDesir}
+                imageAlt='objet du désir'
+                description="Une pièce de théâtre en 3 actes, où le Sujet rencontre l'Objet du désir, sans cesse renouvelé."
+            />
+            <Livre
+                className='livre'
+                title='La clé'
+                image={cle}
+                imageAlt='La clé'
+                description="Le manuel du graphieros, présentant les raisons de la création de cette idéolangue, les fondations de son architecture, les ressources grammaticales, phonologiques, et le lexique."
             />
         </div>
     )
