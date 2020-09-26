e7d17d6a478fee16d5051e3a882458ddb7a827ea
graphieros <alec.lloyd.probert@gmail.com>
2020-09-26 16:54:24 +0200

    _

--- reactversion/source3/src/components/Gallery.js
+++ reactversion/source3/src/components/Gallery.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './css/Gallery.css';
 
 function Gallery() {
     return (
         <div className='gallery-body'>
-            GALERIE
+            <div className='gallery-title'>
+                galerie de peintures
+                <p>écrire et peindre simultanément</p>
+            </div>
         </div>
     )
 }
