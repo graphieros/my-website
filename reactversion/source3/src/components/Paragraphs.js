1f3bf234d98b35b083bca549df99c332bf934c13
graphieros <alec.lloyd.probert@gmail.com>
2020-11-06 17:22:25 +0100

    just a page to contain the paragraph composer

---
+++ reactversion/source3/src/components/Paragraphs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './css/Paragraphs.css';
+import ParagraphGenerator from './ParagraphGenerator';
+
+function Paragraphs() {
+    return (
+        <div className='paragraphs-body'>
+            <ParagraphGenerator />
+        </div>
+    )
+}
+
+export default Paragraphs;
