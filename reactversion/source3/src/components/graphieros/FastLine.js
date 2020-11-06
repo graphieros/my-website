3490042f4c0989cd73c2dd294181f2553ff8e1aa
graphieros <alec.lloyd.probert@gmail.com>
2020-11-06 05:17:47 +0100

    component to use the new fastLine function

---
+++ reactversion/source3/src/components/graphieros/FastLine.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { fastLine } from '../../libraries/graphieros0.2';
+
+//the DIV needs to have a defined css height
+
+const FastLine = (props) => {
+
+    const fl = fastLine({
+        sequence:props.sequence,
+        width: props.width,
+        colors: props.colors
+    });
+
+    return (
+         <div className={props.className} dangerouslySetInnerHTML={{ __html: fl }}></div>
+    );
+}
+
+export default React.memo(FastLine);
