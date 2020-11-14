dd0b37ec3969c9912743dd779b87c6036c5e7556
graphieros <alec.lloyd.probert@gmail.com>
2020-11-14 09:39:00 +0100

    exploding glyphs animation for landing page

---
+++ reactversion/source3/src/components/StarExplosion.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Fractal from './graphieros/Fractal';
+import './css/StarExplosion.css';
+
+const StarExplosion = (props) => {
+    return (
+        <div className={props.className}>
+            <Fractal
+                className='starExplode-0'
+                sequence='zw-wd-dz-qs-se-xs'
+                colors={[255,255,255]} 
+                svgSize='200'
+            />
+            <Fractal
+                className='starExplode-1'
+                sequence='zs-se-ez-wq-qd-dx'
+                colors={[255,255,255]} 
+                svgSize='200'
+            />
+            <Fractal
+                className='starExplode-2'
+                sequence='qd-dw-ex'
+                colors={[255,255,255]} 
+                svgSize='200'
+            />
+            <Fractal
+                className='starExplode-3'
+                sequence='zw-wd-dz-qe-ex-xq'
+                colors={[255,255,255]} 
+                svgSize='200'
+            />
+            <Fractal
+                className='starExplode-4'
+                sequence='qw-wz-sx-xe'
+                colors={[255,255,255]} 
+                svgSize='200'
+            />
+            <Fractal
+                className='starExplode-5'
+                sequence='zx-ws-qe'
+                colors={[255,255,255]} 
+                svgSize='200'
+            />
+        </div>
+    )
+}
+
+export default StarExplosion;
