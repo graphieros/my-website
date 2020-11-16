e4aed71794eeafe19c28b94d8431b15f39e4dd5c
graphieros <alec.lloyd.probert@gmail.com>
2020-11-16 18:39:10 +0100

    Fractal component without styling

---
+++ vueversion/graphieros/src/components/Fractal.vue
@@ -0,0 +1,33 @@
+<template>
+  <div v-html="frac" />
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { callifractal } from "../library/graphieros0.2";
+
+export default defineComponent({
+  name: "Fractal",
+  props: {
+    sequence: String,
+    colors: String,
+    svgSize: String,
+    intensity: String,
+    light: Boolean,
+    size: String
+  },
+  data() {
+    return {
+      frac: callifractal({
+        sequence: this.sequence,
+        colors: this.colors.split(","),
+        svgSize: this.svgSize,
+        intensity: this.intensity,
+        light: Boolean(this.light), //weird hack
+        size: this.size
+      })
+    };
+  }
+});
+</script>
+<style lang="scss" scoped></style>
