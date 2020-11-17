b39860c059b2acaf1362d6825ab549999c77f0a2
graphieros <alec.lloyd.probert@gmail.com>
2020-11-17 06:54:16 +0100

    Molecule component for Vue

---
+++ vueversion/graphieros/src/components/Molecule.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-html="line" />
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { molecular } from "../library/graphieros0.2";
+
+export default defineComponent({
+  name: "Molecule",
+  props: {
+    class: String,
+    sequence: String,
+    colors: String,
+    size: String,
+    strokeWidth: String
+  },
+  data() {
+    return {
+      line: molecular({
+        class: this.class,
+        sequence: this.sequence,
+        colors: this.colors.split(","),
+        size: this.size,
+        strokeWidth: Number(this.strokeWidth)
+      })
+    };
+  }
+});
+</script>
+<style lang="scss" scoped></style>
