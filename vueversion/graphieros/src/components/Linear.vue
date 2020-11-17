6c45aed0cc75f28b9210cf843495ffd9076b5c02
graphieros <alec.lloyd.probert@gmail.com>
2020-11-17 06:54:04 +0100

    Linear component for Vue

---
+++ vueversion/graphieros/src/components/Linear.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-html="line" />
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { fastLine } from "../library/graphieros0.2";
+
+export default defineComponent({
+  name: "Linear",
+  props: {
+    class: String,
+    sequence: String,
+    colors: String,
+    size: String
+  },
+  data() {
+    return {
+      line: fastLine({
+        class: this.class,
+        sequence: this.sequence,
+        colors: this.colors.split(","),
+        size: this.size
+      })
+    };
+  }
+});
+</script>
+<style lang="scss" scoped></style>
