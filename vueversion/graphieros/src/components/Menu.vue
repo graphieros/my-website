71b591bc81215b58c1637179d5ca59b1e2c9bb86
graphieros <alec.lloyd.probert@gmail.com>
2020-11-20 18:04:50 +0100

    menu component, not yet used

---
+++ vueversion/graphieros/src/components/Menu.vue
@@ -0,0 +1,19 @@
+<template>
+  <div className="menu-body">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/glyphes">Glyphes</router-link>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Menu"
+});
+</script>
+
+<style lang="scss" scoped>
+.menu-body {
+    position: absolute;
+}
+</style>
