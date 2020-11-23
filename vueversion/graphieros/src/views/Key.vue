c6cabf1a3794d7d50b53b70df8230955780ace1b
graphieros <alec.lloyd.probert@gmail.com>
2020-11-23 05:39:20 +0100

    la clé view

---
+++ vueversion/graphieros/src/views/Key.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="key-body">
+    LA CLE
+  </div>
+</template>
+
+<script lang="js">
+
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "La clé"
+});
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
