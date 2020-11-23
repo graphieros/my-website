6719a11c26c2e3333bdabcee999ffa41d984730b
graphieros <alec.lloyd.probert@gmail.com>
2020-11-23 05:39:09 +0100

    graphieros editor view

---
+++ vueversion/graphieros/src/views/Editor.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="editor-body">
+    EDITOR
+  </div>
+</template>
+
+<script lang="js">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Editor"
+});
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
