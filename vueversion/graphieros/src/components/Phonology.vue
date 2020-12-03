6f6126477187c55ee28f98e66e8303a484194bea
graphieros <alec.lloyd.probert@gmail.com>
2020-12-03 06:32:43 +0100

    basic implementation

--- vueversion/graphieros/src/components/Phonology.vue
+++ vueversion/graphieros/src/components/Phonology.vue
@@ -2,6 +2,20 @@
   <div class="phonology-body">
     <h1 v-if="selectedLang === 'toggle-right'">phono<span>Logy</span></h1>
     <h1 v-else>phono<span>Logie</span></h1>
+    <div class="phonology-set-wrapper">
+      <div class="phonology-set">
+        <span v-if="selectedLang === 'toggle-right'">consonants</span>
+        <span v-else>consonnes</span>
+
+        <p>
+          [b], [ʃ], [k], [d], [f], [g], [h], [ʒ], [l], [m], [n], [p], [r], [s],
+          [t], [v], [w], [z]
+        </p>
+        <span v-if="selectedLang === 'toggle-right'">vowels</span>
+        <span v-else>voyelles</span>
+        <p>[a], [e], [i], [o], [y], [u]</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -49,6 +63,31 @@ export default defineComponent({
     margin-left: 50%;
     transform: translateX(-50%);
     color: RGB(var(--c3));
+  }
+}
+.phonology-set-wrapper {
+  width: 100%;
+  background: radial-gradient(at top right, black, RGB(var(--c0)));
+  box-sizing: border-box;
+  padding-top: 48px;
+  padding-bottom: 48px;
+}
+.phonology-set {
+  display: grid;
+  max-width: 600px;
+  margin: 0 auto;
+  grid-template-columns: repeat(2, 1fr);
+  align-items: center;
+  justify-items: center;
+  width: 100%;
+  span {
+    color: RGB(var(--c2));
+    text-align: right;
+    width: 100px;
+  }
+  p {
+    text-align: left;
+    font-family: var(--mono0);
   }
 }
 </style>
