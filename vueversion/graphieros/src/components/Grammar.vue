29bd88ef1b1b29451485cc765174d7c656e1285f
graphieros <alec.lloyd.probert@gmail.com>
2020-12-03 06:43:32 +0100

    grammar component

---
+++ vueversion/graphieros/src/components/Grammar.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="grammar-body">
+    <h1 v-if="selectedLang === 'toggle-right'">Grammar</h1>
+    <h1 v-else>Grammaire</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import store from "@/store/index.ts";
+
+export default defineComponent({
+  name: "Grammar",
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {
+    selectedLang() {
+      return store.getters.toggleClass;
+    },
+  },
+  methods: {},
+});
+</script>
+
+<style lang="scss" scoped>
+.grammar-body {
+  width: 100%;
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
+  margin-top: 130px;
+  font-family: var(--logo);
+  transition: all 0.25s ease-in-out;
+  h1 {
+    color: RGB(var(--c3));
+    font-size: 3em;
+    span {
+      color: RGB(var(--c2));
+      display: inline-block;
+    }
+  }
+  p {
+    text-align: center;
+    max-width: 60ch;
+    margin-left: 50%;
+    transform: translateX(-50%);
+    color: RGB(var(--c3));
+  }
+}
+</style>
